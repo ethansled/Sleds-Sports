8d03c9123acb45ab3c6e63e70bfff7ba1d9b70c8
Ethan Sledzinski <ethansled@gmail.com>
2021-12-09 16:19:51 -0500

    added css & rating page

---
+++ public/js/config.js
@@ -0,0 +1,15 @@
+const MY_API_KEY_SOCCER = 'c8858a5cf8mshf52644aba28bdebp1b71a3jsn849ecf57e2d4';
+export { MY_API_KEY_SOCCER };
+
+
+const MY_API_KEY_BASEBALL = 'c8858a5cf8mshf52644aba28bdebp1b71a3jsn849ecf57e2d4';
+export { MY_API_KEY_BASEBALL };
+
+const MY_API_KEY_BASKETBALL = 'c8858a5cf8mshf52644aba28bdebp1b71a3jsn849ecf57e2d4';
+export { MY_API_KEY_BASKETBALL };
+
+const MY_API_KEY_FOOTBALL = 'c8858a5cf8mshf52644aba28bdebp1b71a3jsn849ecf57e2d4';
+export { MY_API_KEY_FOOTBALL };
+
+const MY_API_KEY_HOCKEY = 'c8858a5cf8mshf52644aba28bdebp1b71a3jsn849ecf57e2d4';
+export { MY_API_KEY_HOCKEY };   
